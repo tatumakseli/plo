b2fd9323a305c0effaa23039c4c0e191c61b2dab
tatumakseli <teit91@gmail.com>
2018-12-05 12:25:02 +0200

    working

--- range.py
+++ range.py
@@ -1,12 +1,14 @@
 from treys import Card, Evaluator
 import itertools
+import numpy as np
 
 class Hand:
-	def __init__(self, hand, flop):
-		self.hand = hand
+	def __init__(self, hand1, hand2, flop):
+		self.hand1 = hand1
+		self.hand2 = hand2
 		self.flop = flop
 		self.allcards = Hand.allCards(self)
-		self.cardsLeft = [elem for elem in self.allcards if elem not in self.hand and elem not in self.flop]
+		self.cardsLeft = [elem for elem in self.allcards if elem not in self.hand1 and elem not in self.hand2 and elem not in self.flop]
 		#print(self.cardsLeft)
 		Hand.twoCardHandsFunc(self)
 		Hand.turnAndRiver(self)
@@ -23,23 +25,45 @@ class Hand:
 		return allcards
 
 	def twoCardHandsFunc(self):
-		self.twoCardHands = list(itertools.combinations(self.hand, 2))
+		self.hand1_comb = list(itertools.combinations(self.hand1, 2))
+		self.hand2_comb = list(itertools.combinations(self.hand2, 2))
 		
 	def turnAndRiver(self):
 		self.turnAndRiverCombos = list(itertools.combinations(self.cardsLeft, 2))
-		print(len(self.turnAndRiverCombos))
 
 	def calc(self):
+		hand1_win = []
+		hand2_win = []
+		tie = []
 		evaluator = Evaluator()
-		# board = [Card.new(self.flop[0]), Card.new(self.flop[1]), Card.new(self.flop[2])]
 		for x in self.turnAndRiverCombos:
 			board = [Card.new(self.flop[0]), Card.new(self.flop[1]), Card.new(self.flop[2]), Card.new(x[0]), Card.new(x[1])]
-			for key in self.twoCardHands:
-				hand = [Card.new(key[0]), Card.new(key[1])]
-				print(evaluator.evaluate(board, hand))
+			hand1_best = None
+			for key in self.hand1_comb:
+				hand1 = [Card.new(key[0]), Card.new(key[1])]
+				score = evaluator.evaluate(board, hand1)
+				if hand1_best is None or score < hand1_best:
+					hand1_best = score
+			hand2_best = None
+			for key in self.hand2_comb:
+				hand2 = [Card.new(key[0]), Card.new(key[1])]
+				score = evaluator.evaluate(board, hand2)
+				if hand2_best is None or score < hand2_best:
+					hand2_best = score
+			if hand1_best == hand2_best:
+				tie.append(1)
+			elif hand1_best > hand2_best:
+				hand2_win.append(1)
+			elif hand1_best < hand2_best:
+				hand1_win.append(1)
+		total = sum(hand1_win)+sum(hand2_win)+sum(tie)
+		print("TIE", sum(tie)/total)
+		print("HAND1WIN", sum(hand1_win)/total)
+		print("HAND2WIN", sum(hand2_win)/total)
 
 
-test = Hand(["9d", "8d", "7c", "6c"], ["As", "Kc", "Jh"])
+
+test = Hand(["Ad", "Ac", "Td", "9c"], ["Ks", "Kh", "Th", "Qs"], ["2c", "2s", "5d"])
 
 """
 board = [
